3b841c9905246e163abf1458abaf3e5403b53ade
Yifu Li[yl3234] <yveslyf@gmail.com>
2019-11-06 17:23:02 -0500

    add docker specifications, should be deployable

---
+++ backend/app/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, render_template
+
+
+app = Flask(__name__, template_folder='templates')
+
+
+@app.route('/')
+def landing_page():
+    return render_template("index.html"), 200
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
