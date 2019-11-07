51edc8c0a2dc3b5dd3e77200a7a07f170e6b93e0
Yifu Li[yl3234] <yveslyf@gmail.com>
2019-11-07 12:49:41 -0500

    modified landing page

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template
 
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder='../../frontend/templates')
 
 
 @app.route('/')
