b55e61b56834168dc2c388339ec794867bfde397
Yifu Li[yl3234] <yveslyf@gmail.com>
2019-11-15 16:55:55 -0500

    add app.py

--- app/__init__.py
+++ app/__init__.py
@@ -22,5 +22,3 @@ app.register_blueprint(index)
 from app.api import api
 app.register_blueprint(api, url_prefix='/api')
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
